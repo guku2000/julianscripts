1f3b6ec590193b99a8691716d73e3c88e93db38c
guku2000 <julian@gojulian.com>
2016-09-27 19:42:28 -0500

    trying to fix
    switched to make faster...

--- crack_t1.js
+++ crack_t1.js
@@ -7,25 +7,24 @@ function(context, args)
 	var e=["open", "release", "unlock"];
 	var t = args.t
 	var r = t.call({});
+	var ld = 0;
+	var rs = r.split("\n")[ld];
+	var rd = r.split("\n")[ld+1];
 	var v = {};
 	var i = 0;
 	var k = 9;
-	var to = false;
-	var tf = false;
-	while (r.substr() == -1){
-		if (r.indexOf("EZ_21") != -1 ){
-			while (r.indexOf("EZ_21") == -1 && to == false){
+	while (rs.substr(1,10)== "LOCK_ERROR"){
+
+		if (rd.indexOf("EZ_21") != -1 ){
+			while (rs.substr (6,19) != "UNLOCKED EZ_21" || r.substr (16,35) != "Connection terminated"){
 				v["EZ_21"]= e[i];
 				r = t.call(v);
+				rs = r.split("\n")[ld];
 				i++;
-				return "yay1"
 			}
-			to = true
-			i = 0;
 		}
 		
-		else if (r.indexOf("EZ_35") > -1 && tf== false){
-			return "yay"
+		else if (rd.indexOf("EZ_35") != -1){
 			while (r.indexOf("digit") == -1){
 				v["EZ_35"]= e[i];
 				r = t.call(v);
@@ -36,12 +35,14 @@ function(context, args)
 				r= t.call(v);
 				k--;
 			}
-			tf = true
-			i=0;
-			k=9;
 		}
 		
-		
+		ld++;
+		rs = r.split("\n")[ld];
+		rd = r.split("\n")[ld+1];
+		i=0;
+		k=9;
 		
 	}
+return r
 }
